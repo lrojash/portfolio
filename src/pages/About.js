c6dbf55fd709322409e7f0872a63b83d5237bc8f
Luis Rojas <luisrohu91@gmail.com>
2021-05-03 08:41:46 -0600

    change

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ function About() {
     return (
         <div className="about" id="about">
             <div className="text" style={{marginTop:'7vh'}}>
-                <h1 style={{color: 'white'}}>About Me</h1>
+                <h1 style={{color: 'black'}}>About Me</h1>
                 <p>Hello! I am Luis, a software engineer based in Denver, CO.<br /><br />
                I enjoy tackling difficult problems while providing simple solutions that anyone can understand. As a developer I aim to create software that is visual appealing and fast.
                These characteristics are what make any application a long-term success.
