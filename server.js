3aae65fd8797b4c31200a8a2b9a49a9ee7543668
lrojash <luisrohu91@gmail.com>
2021-05-31 18:30:15 +0000

    add script

--- server.js
+++ server.js
@@ -1,34 +1,3 @@
-import { datadogRum } from '@datadog/browser-rum';
-import { datadogLogs } from '@datadog/browser-logs';
-
-// Datadog tracing to APM
-const tracer = require('dd-trace').init({
-    logInjection: true
-});
-require('dotenv').config();
-
-// Adding logs
-
-datadogLogs.init({
-     clientToken: process.env.TOKEN,
-     site: 'datadoghq.com',
-     forwardErrorsToLogs: true,
-     sampleRate: 100
- });
-// Adding Broswer
-datadogRum.init({
-    applicationId: '57a2ee76-6934-442d-9fae-aa40d743e7aa',
-    clientToken: 'pubec59d654b295b7a7670be20ea9688e8f',
-    site: 'datadoghq.com',
-    service:'-https://lrojasportfolio.herokuapp.com/',
-    // Specify a version number to identify the deployed version of your application in Datadog 
-    version: '1.0.0',
-    sampleRate: 100,
-    trackInteractions: true
-});
-
-
-
 const express = require('express');
 const app = express();
 const nodemailer = require('nodemailer');
